034c854cf659f29f4056444e1b9b892975afcf10
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2022-05-06 08:44:10 -0300

    Início de strings.1 - criptografia

---
+++ exercicios/3-strings/strings.1-criptografia/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        /*Lembrar: estudar hashmap e tabela ASCII em java*/
+        char a = 'A';
+
+        System.out.println((int) a);
+ 
+    }
+ 
+}
