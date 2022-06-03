f8b34a2ac5a6a768b4bb29e7cbb15c250a3ea179
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2022-06-03 12:20:55 -0300

    BEE 1028 finalizado
    Comentários adicionados

--- exercicios/5-matematica/bee1028-figurinhas/src/Main.java
+++ exercicios/5-matematica/bee1028-figurinhas/src/Main.java
@@ -1,3 +1,21 @@
+/*
+Autor: Enock Bezerra Ferreira de Souza.
+Problema: BEE 1028.
+Título: Figurinhas.
+
+Descrição
+
+Ricardo e Vicente são aficionados por figurinhas. Nas horas vagas, eles arrumam um jeito de jogar um “bafo” ou algum outro jogo que envolva tais figurinhas. Ambos também têm o hábito de trocarem as figuras repetidas com seus amigos e certo dia pensaram em uma brincadeira diferente. Chamaram todos os amigos e propuseram o seguinte: com as figurinhas em mãos, cada um tentava fazer uma troca com o amigo que estava mais perto seguindo a seguinte regra: cada um contava quantas figurinhas tinha. Em seguida, eles tinham que dividir as figurinhas de cada um em pilhas do mesmo tamanho, no maior tamanho que fosse possível para ambos. Então, cada um escolhia uma das pilhas de figurinhas do amigo para receber. Por exemplo, se Ricardo e Vicente fossem trocar as figurinhas e tivessem respectivamente 8 e 12 figuras, ambos dividiam todas as suas figuras em pilhas de 4 figuras (Ricardo teria 2 pilhas e Vicente teria 3 pilhas) e ambos escolhiam uma pilha do amigo para receber.
+
+Entrada
+
+A primeira linha da entrada contém um único inteiro N (1 ≤ N ≤ 3000), indicando o número de casos de teste. Cada caso de teste contém 2 inteiros F1 (1 ≤ F1 ≤ 1000) e F2 (1 ≤ F2 ≤ 1000) indicando, respectivamente, a quantidade de figurinhas que Ricardo e Vicente têm para trocar.
+
+Saída
+
+Para cada caso de teste de entrada haverá um valor na saída, representando o tamanho máximo da pilha de figurinhas que poderia ser trocada entre dois jogadores.
+*/
+
 import java.util.Scanner;
 
 public class Main
@@ -19,9 +37,6 @@ public class Main
 
     /**
      * Realiza o mdc entre os números f1 e f2 usando o algoritmo de Euclides.
-     * @param f1
-     * @param f2
-     * @return
      */
     static int mdc(int f1, int f2)
     {
@@ -43,38 +58,4 @@ public class Main
 
         return maior;
     }
-
-    /**
-     * Calcula o Máximo Divisor Comum (MDC) de dois números passados como parâmetros. (DEPRECATED)
-     * @param a Numerador.
-     * @param b Denominador.
-     * @return MDC inteiro.
-     
-    static int mdc(int a, int b)
-    {
-
-        int max = a;
-        if (b > max) { max = b; }
-        
-        int mdc = 1;
-        int div = 2;
-
-        do
-        {
-            if ((a % div == 0) && (b % div == 0))
-            {
-                mdc *= div;
-                a /= div;
-                b /= div;
-            }else
-            {
-                div++;
-            }
-           
-        }while(div <= max);
-
-        return mdc;
-
-    }
-    */
 }
